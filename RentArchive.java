2204aea7fa3f4569a061f5a777bf257f36ebcd19
candrasaga <103479361+candrasaga@users.noreply.github.com>
2022-06-18 18:16:23 +0700

    menambahkan file di github

---
+++ RentArchive.java
@@ -0,0 +1,38 @@
+package tugasarrayList;
+
+import java.util.ArrayList;
+
+public class RentArchive {
+    ArrayList<CarRent> rentData;
+
+    public RentArchive() {
+        rentData = new ArrayList<CarRent>();
+    }
+
+    public void Rent(CarRider rider, Car car, int rentDur) {
+
+        if (car.isStatus() == true) {
+            System.out.println("MOBIL BERHASIL DISEWA");
+            car.setStatus(false);
+            rentData.add(new CarRent(rider, car, rentDur));
+        } else
+            System.out.println("MAAF, MOBIL SUDAH DISEWA");
+    }
+
+    public void info() {
+        System.out.println("------------------------------");
+        System.out.println("INFORMASI PELANGGAN");
+        System.out.println("------------------------------");
+
+        for (CarRent i : rentData) {
+            System.out.println("NAMA PEMINJAM : " + i.getRider().getName());
+            System.out.println("TIPE MOBIL : " + i.getCar().getCarType());
+            System.out.println("NO. POLISI : " + i.getCar().getPolNum());
+            System.out.println("LAMA RENTAL : " + i.getRentDur());
+            System.out.println("------------------------------");
+
+        }
+
+    }
+
+}
