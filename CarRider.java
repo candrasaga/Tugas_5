07b2feb5bd4467a7f49823eaf05698b9f3507b4a
candrasaga <103479361+candrasaga@users.noreply.github.com>
2022-06-18 18:14:54 +0700

    menambahkan file di github

---
+++ CarRider.java
@@ -0,0 +1,37 @@
+package tugasarrayList;
+
+public class CarRider {
+    private String name;
+    private int age;
+    private String phone;
+
+    public CarRider(String name, int age, String phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
